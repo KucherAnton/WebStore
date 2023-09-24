34110a0b0e57cd1a23543ec7a27e91d16d14de6e
Антон Кучер <kapetrovi4@gmail.com>
2023-09-24 05:20:18 +0300

    ending backend, starting frontend

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,12 +1,13 @@
-const { Device } = require('../models/models');
+const { Device, DeviceInfo } = require('../models/models');
 const ApiError = require('../error/apiError');
 const uuid = require('uuid');
 const path = require('path');
 
 class DeviceController {
 	async create(req, res) {
-		const { name, price, brandId, typeId, info } = req.body;
+		let { name, price, brandId, typeId, info } = req.body;
 		const { img } = req.files;
+
 		let fileName = uuid.v4() + '.jpg';
 		img.mv(path.resolve(__dirname, '..', 'static', fileName));
 
@@ -17,6 +18,17 @@ class DeviceController {
 			typeId,
 			img: fileName,
 		});
+
+		if (info) {
+			info = JSON.parse(info);
+			info.forEach((i) =>
+				DeviceInfo.create({
+					title: i.title,
+					description: i.description,
+					deviceId: device.id,
+				})
+			);
+		}
 
 		return res.json(device);
 	}
@@ -29,19 +41,27 @@ class DeviceController {
 
 		let devices;
 		if (!brandId && !typeId) {
-			devices = await Device.findAll({ limit, offset });
+			devices = await Device.findAndCountAll({ limit, offset });
 		}
 
 		if (brandId && !typeId) {
-			devices = await Device.findAll({ where: { brandId }, limit, offset });
+			devices = await Device.findAndCountAll({
+				where: { brandId },
+				limit,
+				offset,
+			});
 		}
 
 		if (!brandId && typeId) {
-			devices = await Device.findAll({ where: { typeId }, limit, offset });
+			devices = await Device.findAndCountAll({
+				where: { typeId },
+				limit,
+				offset,
+			});
 		}
 
 		if (brandId && typeId) {
-			devices = await Device.findAll({
+			devices = await Device.findAndCountAll({
 				where: { brandId, typeId },
 				limit,
 				offset,
@@ -51,7 +71,15 @@ class DeviceController {
 		return res.json(devices);
 	}
 
-	async getOne(req, res) {}
+	async getOne(req, res) {
+		const { id } = req.params;
+		const device = await Device.findOne({
+			where: { id },
+			include: [{ model: DeviceInfo, as: 'info' }],
+		});
+
+		return res.json(device);
+	}
 }
 
 module.exports = new DeviceController();
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,16 +1,57 @@
+const { User, Basket } = require('../models/models');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const ApiError = require('../error/apiError');
 
-class UserController {
-	async registration(req, res) {}
+const generateJwt = (id, email, role) => {
+	return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+		expiresIn: '24h',
+	});
+};
 
-	async login(req, res) {}
+class UserController {
+	async registration(req, res, next) {
+		const { email, password, role } = req.body;
+		if (!email || !password) {
+			return next(ApiError.badReq('Некорректный email или пароль'));
+		}
+
+		const candidate = await User.findOne({ where: { email } });
+		if (candidate) {
+			return next(ApiError.badReq('Пользователь уже существует'));
+		}
+
+		const hashPassword = await bcrypt.hash(password, 5);
+
+		const user = User.create({ email, password, password: hashPassword });
+		const basket = Basket.create({ userId: user.id });
+
+		const token = generateJwt(user.id, user.email, user.role);
+
+		return res.json({ token });
+	}
+
+	async login(req, res, next) {
+		const { email, password } = req.body;
+
+		const user = await User.findOne({ where: { email } });
+		if (!user) {
+			return next(ApiError.internal('Пользователь не найден'));
+		}
+
+		let comparePassword = bcrypt.compareSync(password, user.password);
+		if (!comparePassword) {
+			return next(ApiError.internal('Пароль неверный'));
+		}
+
+		const token = generateJwt(user.id, user.email, user.role);
+
+		return res.json({ token });
+	}
 
 	async check(req, res, next) {
-		const { id } = req.query;
-		if (!id) {
-			return next(ApiError.badReq('Не задан ID'));
-		}
-		res.json(id);
+		const token = generateJwt(req.user.id, req.user.email, req.user.role);
+		res.json({ token });
 	}
 }
 
