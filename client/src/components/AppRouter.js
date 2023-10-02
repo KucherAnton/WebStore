8f198d75aa46ae88d928653e4c35186207c44278
Антон Кучер <kapetrovi4@gmail.com>
2023-10-02 19:43:09 +0300

    github pages test 1

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Routes, Route, Redirect } from 'react-router-dom';
+import { authRoutes, publicRoutes } from '../routes';
+
+const AppRouter = () => {
+	const isAuth = false;
+
+	return (
+		<Routes>
+			{isAuth &&
+				authRoutes.map(({ path, Component }) => {
+					<Route key={path} path={path} element={Component} />;
+				})}
+			{publicRoutes.map(({ path, Component }) => {
+				<Route key={path} path={path} element={Component} />;
+			})}
+			<Route>PENIS</Route>
+		</Routes>
+	);
+};
+
+export default AppRouter;
